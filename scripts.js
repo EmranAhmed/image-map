192c47c880fffe58dfbc3c90e5726abef315ea00
Emran Ahmed <emran.bd.08@gmail.com>
2023-04-08 08:38:53 +0600

    Improved

--- scripts.js
+++ scripts.js
@@ -1,99 +1,169 @@
-// To maintain responsive spot you can only customize wide of image-wrapper
-// and image wrapper width and image width should be same
-// Don't apply padding or margin on image
+(function (window) {
 
-(function () {
+  'use strict'
 
-  const imageWrapper = document.querySelector('#image-wrapper')
-  const { left, top, width, height } = imageWrapper.getBoundingClientRect()
+  const weakMap = new WeakMap()
 
-  let currentPointer = null
+  const Plugin = (() => {
 
-  function calculatePointerPosition (event) {
+    return class {
 
-    const pointerWidth = 10
-
-    // const { left, top, width, height } = imageWrapper.getBoundingClientRect()
-
-    let x = ((event.x - left - pointerWidth) / width) * 100
-    let y = ((event.y - top - pointerWidth) / height) * 100
-
-    return { x, y }
-  }
-
-  function setPointer (event) {
-
-    if (event.button > 0) {
-      return
-    }
-
-    if (isPointer(event)) {
-      return
-    }
-
-    const { x, y } = calculatePointerPosition(event)
-
-    imageWrapper.insertAdjacentHTML(
-      'beforeend',
-      `<span class="pointer" style="--left: ${x}%; --top: ${y}%"></span>`,
-    )
-
-  }
-
-  function preparePointer (event) {
-    if (event.button > 0) {
-      return
-    }
-
-    if (isPointer(event)) {
-      imageWrapper.addEventListener('pointermove', movePointer)
-    }
-  }
-
-  function getSelectedPointer (event) {
-    const pointers = imageWrapper.querySelectorAll('.pointer')
-
-    pointers.forEach((pointer) => {
-
-      if (pointer.contains(event.target)) {
-        currentPointer = pointer
-        return pointer
+      DEFAULTS = {
+        pointerSize: 20,
       }
-    })
 
-    return currentPointer
-  }
+      constructor (element, options, name) {
 
-  function isPointer (event) {
-    const pointer = getSelectedPointer(event)
-    return pointer?.contains(event.target)
-  }
+        this.element = element
+        this.settings = Object.assign({}, this.DEFAULTS, options)
+        this.currentPointer = null
+        this.name = name
+        this.rect = this.element.getBoundingClientRect()
 
-  function movePointer (event) {
+        this.init()
+      }
 
-    event.preventDefault()
-    const { x, y } = calculatePointerPosition(event)
-    imageWrapper.classList.add('moving')
-    currentPointer.style.setProperty('--left', `${x}%`)
-    currentPointer.style.setProperty('--top', `${y}%`)
-  }
+      init () {
 
-  function stopMovingPointer (event) {
+        this.element.addEventListener('pointerdown', this.addPointer)
+        this.element.addEventListener('pointerdown', this.preparePointer)
+        this.element.addEventListener('pointerup', this.stopMovingPointer)
+      }
 
-    //console.log('xx')
-    currentPointer = null
-    imageWrapper.classList.remove('moving')
-    imageWrapper.removeEventListener('pointermove', movePointer)
-  }
+      addPointer = (event) => {
 
-  imageWrapper.addEventListener('pointerdown', setPointer)
+        if (event.button > 0 || this.isPointer(event)) {
+          return
+        }
+       
 
-  imageWrapper.addEventListener('pointerdown', preparePointer)
-  imageWrapper.addEventListener('pointerup', stopMovingPointer)
+        const { x, y } = this.calculatePointerPosition(event)
 
-  imageWrapper.addEventListener('pointercancel', (event) => {
+        const template = `<span class="pointer" style="--size:${this.settings.pointerSize}px; --left: ${x}%; --top: ${y}%"></span>`
 
-    console.log('cancel')
-  })
+        this.element.insertAdjacentHTML('beforeend', template)
+      }
 
-})()
+      preparePointer = (event) => {
+
+        if (event.button > 0) {
+          return
+        }
+
+        if (this.isPointer(event)) {
+          this.element.addEventListener('pointermove', this.movePointer)
+        }
+      }
+
+      movePointer = (event) => {
+
+        event.preventDefault()
+
+        const { x, y } = this.calculatePointerPosition(event)
+
+        this.element.classList.add('moving')
+        this.currentPointer.style.setProperty('--left', `${x}%`)
+        this.currentPointer.style.setProperty('--top', `${y}%`)
+      }
+
+      stopMovingPointer = (event) => {
+        this.currentPointer = null
+        this.element.classList.remove('moving')
+        this.element.removeEventListener('pointermove', this.movePointer)
+      }
+
+      getSelectedPointer (event) {
+        const pointers = this.element.querySelectorAll('.pointer')
+
+        for (const pointer of pointers) {
+          if (pointer.contains(event.target)) {
+            this.currentPointer = pointer
+            return pointer
+          }
+        }
+      }
+
+      calculatePointerPosition (event) {
+
+        const { left, top, width, height } = this.rect
+
+        let x = ((event.x - left - (this.settings.pointerSize / 2)) / width) *
+          100
+        let y = ((event.y - top - (this.settings.pointerSize / 2)) / height) *
+          100
+
+        return { x, y }
+      }
+
+      isPointer (event) {
+        const pointer = this.getSelectedPointer(event)
+        return pointer?.contains(event.target)
+      }
+
+      setPointerSize (width) {
+        this.settings.pointerSize = width
+      }
+
+      getPointerSize () {
+        return this.settings.pointerSize
+      }
+
+      destroy () {
+
+        this.element.removeEventListener('pointerdown', this.addPointer)
+        this.element.removeEventListener('pointerdown', this.preparePointer)
+        this.element.removeEventListener('pointermove', this.movePointer)
+        this.element.removeEventListener('pointerup', this.stopMovingPointer)
+
+        delete window[this.name]
+
+        weakMap.delete(this.element)
+      }
+    }
+
+  })()
+
+  const CreateInstance = (() => {
+
+    return (PluginName, ClassName) => {
+
+      window[PluginName] = function (elements, options, ...args) {
+
+        for (const element of document.querySelectorAll(elements)) {
+
+          let data = weakMap.get(element)
+
+          if (!weakMap.has(element)) {
+            data = new ClassName(element, Object.assign({}, options),
+              PluginName)
+            weakMap.set(element, data)
+          }
+
+          if (typeof options === 'string') {
+
+            if (typeof data[options] === 'object') {
+              return data[options]
+            }
+
+            if (typeof data[options] === 'function') {
+              return data[options](...args)
+            }
+          }
+
+        }
+      }
+    }
+
+  })()
+
+  CreateInstance('ImageMAP', Plugin)
+
+  ImageMAP('#image-wrapper')
+
+  // ImageMAP('#image-wrapper', 'setPointerSize', 30)
+
+  // console.log( ImageMAP('#image-wrapper', 'getPointerSize') )
+
+  // ImageMAP('#image-wrapper', 'destroy')
+
+})(window)
